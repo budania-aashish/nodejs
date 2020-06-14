f2de38511b8779cb20431cb8a26aecbd12d93c74
budania-aashish <budania.aashish@gmail.com>
2020-06-14 21:04:47 +0530

    Email module using Node.js

---
+++ Email/sendMail.js
@@ -0,0 +1,28 @@
+var nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+	service : 'gmail', 
+	auth :{
+		user : 'your email', 
+		pass : 'your_password'
+	}
+});
+
+
+var mailOptions = {
+	from: 'your_mail', 
+	to: 'to_mail', 
+	cc: 'cc_mail',
+	bcc: 'bcc_mail',
+	subject: 'sending mail using Node.js',
+	text : 'seninding mail from node js is easy, use nodemailer'
+};
+
+
+transporter.sendMail(mailOptions, function (err, info){
+	if(err){
+		throw err; 
+	} else{
+		console.log('Email sent : '+ info.response); 
+	}
+});
